17804f97c7cb6ce636ba20d225bf1f86af2a267b
rihan_a <a.rihan@live.com>
2023-04-17 17:04:26 +0200

    add logic to filter user by search query

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,18 +6,25 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.model';
 import { UsersService } from './users.service';
+import { getUsersFilterDto } from './dto/get-users-filter.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  getAllUsers(): User[] {
-    return this.usersService.getAllUsers();
+  getUsers(@Query() filterDto: getUsersFilterDto): User[] {
+    if (Object.keys(filterDto).length) {
+      // service function to filter
+      return this.usersService.getUsersWithFilter(filterDto);
+    } else {
+      return this.usersService.getAllUsers();
+    }
   }
 
   @Get('/:id')
