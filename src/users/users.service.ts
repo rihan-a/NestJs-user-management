7b17ee14a3b164546e0907d6b17caaa1134a3078
rihan_a <a.rihan@live.com>
2023-04-17 17:05:16 +0200

    create getUsersByFilter function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateUserDto } from './dto/create-user.dto';
+import { getUsersFilterDto } from './dto/get-users-filter.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,6 +10,34 @@ export class UsersService {
 
   getAllUsers(): User[] {
     return this.users;
+  }
+
+  getUsersWithFilter(filterDto: getUsersFilterDto): User[] {
+    const { gender, search } = filterDto;
+
+    // define temp array to hold results
+    let users = this.getAllUsers();
+    // filter with gender
+    if (gender) {
+      users = users.filter((user) => user.gender === gender);
+    }
+
+    // filter with search
+    if (search) {
+      users = users.filter((user) => {
+        if (
+          user.name.includes(search) ||
+          user.bio.includes(search) ||
+          user.city.includes(search)
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+    return users;
   }
 
   getUserById(id: string): User {
