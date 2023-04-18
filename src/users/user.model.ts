1b344ff0d9deac2f55c2272bf015852694a0c320
rihan_a <a.rihan@live.com>
2023-04-18 14:40:14 +0200

    use enum instead of type

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -2,11 +2,17 @@ export interface User {
   id: string;
   name: string;
   age: number;
-  gender: userGender;
+  gender: UserGender;
   height: number;
   education: string;
   bio: string;
   city: string;
 }
 
-export type userGender = 'female' | 'male' | 'other';
+//export type userGender = 'female' | 'male' | 'other';
+
+export enum UserGender {
+  FEMALE = 'FEMALE',
+  MALE = 'MALE',
+  OTHER = 'OTHER',
+}
