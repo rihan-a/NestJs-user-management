b0e5abff9ce3c291c2a7f87ce0a04c26600c69a8
rihan_a <a.rihan@live.com>
2023-04-18 17:04:40 +0200

    add validation

--- src/users/dto/get-users-filter.dto.ts
+++ src/users/dto/get-users-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { UserGender } from '../user.model';
 
 export class getUsersFilterDto {
+  @IsOptional()
+  @IsEnum(UserGender)
   readonly gender?: UserGender;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   readonly search?: string;
 }
