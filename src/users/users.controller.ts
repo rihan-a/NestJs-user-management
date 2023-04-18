e6149afc66204ffa8ea6f9f36f14c2c923da90f5
rihan_a <a.rihan@live.com>
2023-04-18 14:40:23 +0200

    add comments

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,32 +17,37 @@ import { getUsersFilterDto } from './dto/get-users-filter.dto';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  // Get all Users or get user by search filter
   @Get()
   getUsers(@Query() filterDto: getUsersFilterDto): User[] {
     if (Object.keys(filterDto).length) {
-      // service function to filter
+      // service function to filter users by search query
       return this.usersService.getUsersWithFilter(filterDto);
     } else {
       return this.usersService.getAllUsers();
     }
   }
 
+  // get User by Id Route
   @Get('/:id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.getUserById(id);
   }
 
+  // Create User Route
   @Post()
   createUser(@Body() CreateUserDto: CreateUserDto) {
-    console.log(CreateUserDto);
+    //console.log(CreateUserDto);
     return this.usersService.createUser(CreateUserDto);
   }
 
+  // Delete User Route
   @Delete('/:id')
   deleteUserById(@Param('id') id: string): string {
     return this.usersService.deleteUserById(id);
   }
 
+  // Update User Route
   @Patch('/:id/name')
   updateUserNameById(
     @Param('id') id: string,
