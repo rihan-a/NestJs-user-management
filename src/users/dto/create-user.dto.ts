d57bf607b22f5f9b006dee1462ae564eb2e17644
rihan_a <a.rihan@live.com>
2023-04-18 11:30:15 +0200

    install class-validator & class-transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,21 @@
 import { userGender } from '../user.model';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   readonly id: string;
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
   readonly age: number;
+
+  @IsNotEmpty()
   readonly gender: userGender;
+
   readonly height: number;
   readonly education: string;
   readonly bio: string;
+
+  @IsNotEmpty()
   readonly city: string;
 }
